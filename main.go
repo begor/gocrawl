6a0a9be04d71f556f84a9a2cab3a20195d90ec88
Egor Biriukov <b-egor@yandex-team.ru>
2019-04-28 14:57:36 +0300

    Modules

--- main.go
+++ main.go
@@ -3,35 +3,15 @@ package main
 import (
 	"fmt"
 	"log"
-	"sync"
 
 	"net/http"
 	"net/url"
 
+	"gocrawl/sitemap"
+
 	"github.com/PuerkitoBio/goquery"
 )
 
-type SyncSet struct {
-	state map[string]bool
-	lock sync.RWMutex
-}
-
-func NewSyncSet() SyncSet {
-	return SyncSet{state: make(map[string]bool), lock: sync.RWMutex{}}
-}
-
-func (s *SyncSet) Add(el string) {
-	s.lock.Lock()
-	s.state[el] = true
-	s.lock.Unlock()
-}
-
-func (s *SyncSet) Contains(el string) bool {
-	s.lock.RLock()
-	_, ok := s.state[el]
-	s.lock.RUnlock()
-	return ok
-}
 
 
 func FetchDocument(url string) *goquery.Document {
@@ -77,8 +57,7 @@ func GetLinks(base_url_str string, doc *goquery.Document) ([]string) {
 			}
 
 			base_u, _ := url.Parse(base_url_str)
-			new_url := url.URL{Scheme: base_u.Scheme, Host: base_u.Host, Path: u.Path}
-			links = append(links, new_url.String())
+			links = append(links, base_u.ResolveReference(u).String())
 		}
 	}
 
@@ -86,46 +65,51 @@ func GetLinks(base_url_str string, doc *goquery.Document) ([]string) {
 }
 
 
-func Crawl(url_str string, visited *SyncSet, done_channel chan bool) {
+func Crawl(url_str string, sitemap *sitemap.Sitemap, concurrency_limiter chan int) {
 	fmt.Println("Crawling: ", url_str)
+	fmt.Println("Visited so far: ", sitemap.Size())
 	
-	if visited.Contains(url_str) {
+	if sitemap.Contains(url_str) {
 		fmt.Println(url_str, " already visited")
-		done_channel <- true
 		return
 	}
 
-	visited.Add(url_str)
+	sitemap.Add(url_str)
+
+	concurrency_limiter <- 1
 	
 	doc := FetchDocument(url_str)
+
+	<-concurrency_limiter
+
 	urls := GetLinks(url_str, doc)
 
-	fmt.Println("Got ", len(urls), " links")
+	fmt.Println("Got ", len(urls), " links from ", url_str)
 
-	child_done_channel := make(chan bool)
-
-	for _, url := range urls {
-		go Crawl(url, visited, child_done_channel)
-	}
+	children_queue := make(chan bool)
 
 	for _, url := range urls {
-		fmt.Println("Awaiting: ", url)
-		<-child_done_channel
+		go func (u string) {
+			Crawl(u, sitemap, concurrency_limiter)
+			children_queue <- true
+		}(url)
+	}
+
+	for range urls {
+		<-children_queue
 	}
 
 
-	done_channel <- true
-
-	fmt.Println("Crawled ", url_str, " !")
+	fmt.Println("Crawled ", url_str, "!")
 }
 
 
 func main() {
 	url := "https://monzo.com"
-	visited := NewSyncSet()
-	done_channel := make(chan bool)	
+	sitemap := sitemap.Create()
+	concurrency_limiter := make(chan int, 1000)
 	
-	Crawl(url, &visited, done_channel)
+	Crawl(url, &sitemap, concurrency_limiter)
 
-	<-done_channel
+	sitemap.Print()
 }
---
+++ sitemap/sitemap.go
@@ -0,0 +1,51 @@
+package sitemap
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Sitemap struct {
+	urls map[string]bool
+	lock sync.RWMutex
+}
+
+func Create() Sitemap {
+	return Sitemap{urls: make(map[string]bool), lock: sync.RWMutex{}}
+}
+
+func (s *Sitemap) Add(url string) {
+	s.lock.Lock()
+	s.urls[url] = true
+	s.lock.Unlock()
+}
+
+func (s *Sitemap) Contains(url string) bool {
+	s.lock.RLock()
+	_, ok := s.urls[url]
+	s.lock.RUnlock()
+	
+	return ok
+}
+
+func (s *Sitemap) Size() int {
+	s.lock.RLock()
+	size := len(s.urls)
+	s.lock.RUnlock()
+
+	return size
+}
+
+
+func (s *Sitemap) Print() {
+	s.lock.RLock()
+
+	urls := make([]string, s.Size())
+	for k, _ := range s.urls {
+		urls = append(urls, k)
+	}
+
+	s.lock.RUnlock()
+
+	fmt.Println(urls)
+}
